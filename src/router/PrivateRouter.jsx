848801124c9442a82ed9595ead71d262af6d89a1
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-28 13:57:09 +0100

    recipeApp end

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,11 +1,8 @@
-
 import { Outlet, Navigate } from "react-router-dom";
 import CardProvider, { useCardContext } from "../context/CardProvider";
 
 const PrivateRouter = () => {
   const { name, pass } = useCardContext(CardProvider);
-  console.log(name);
-  console.log(pass);
 
   return name === "cihan" && pass === "1234" ? (
     <Outlet />
